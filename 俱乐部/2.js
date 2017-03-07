35787b38e71308abff27f0588316a5b18fe5a77f
cwh <changwh20052008@sina.com>
2017-03-07 22:53:35 +0800

    test

---
+++ 俱乐部/2.js
@@ -0,0 +1,42 @@
+﻿var id;
+$(document).ready(function(){
+    $(".tabTitleUnHovered,.tabTitleHovered").hover(function(){
+        id=$(this).attr("id");
+    });
+
+})
+
+function tabPageControlHover(n)
+
+{
+    for (var i = 0; i < tabTitles.cells.length; i++)
+    {
+        tabTitles.cells[i].className = "tabTitleUnHovered";
+    }
+    tabTitles.cells[n].className = "tabTitleHovered";
+}
+
+function tabPageControlClick(n)
+
+{
+    var j=0;
+    for (var i = 0; i < tabPagesContainer.tBodies.length; i++)
+    {
+        tabPagesContainer.tBodies[i].className = "tabPageUnSelected";
+        while(j<1){
+            var left1=500*(Number(id)-1);
+            $(".slider").animate({left:left1},300);
+            j++;
+        }
+    }
+    tabPagesContainer.tBodies[n].className = "tabPageSelected";
+}
+function tabPageControlOut(n)
+
+{
+    for (var i = 0; i < tabTitles.cells.length; i++)
+    {
+        tabTitles.cells[i].className = "tabTitleUnHovered";
+    }
+    tabTitles.cells[n].className = "tabTitleUnHovered";
+}
